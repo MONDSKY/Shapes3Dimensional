5f6b378b896b6fac0f434c56268ebd1f9527bb7c
MONDSKY <r6544083@gmail.com>
2025-10-25 07:07:04 +0800

    Update Tetrahedron.java

--- src/Tetrahedron.java
+++ src/Tetrahedron.java
@@ -1,1 +1,34 @@
+public class Tetrahedron extends ThreeDimensionalShape {
+    private double edge;
+
+    public Tetrahedron(double edge) {
+        setEdge(edge);
+    }
+
+    public double getEdge() {
+        return edge;
+    }
+
+    public void setEdge(double edge) {
+        if (edge <= 0) {
+            throw new IllegalArgumentException("Edge length must be positive.");
+        }
+        this.edge = edge;
+    }
+
+    @Override
+    public String getName() {
+        return "Tetrahedron";
+    }
+
+    @Override
+    public double getArea() {
+        return Math.sqrt(3) * Math.pow(edge, 2);
+    }
+
+    @Override
+    public double getVolume() {
+        return Math.pow(edge, 3) / (6 * Math.sqrt(2));
+    }
+}
 
