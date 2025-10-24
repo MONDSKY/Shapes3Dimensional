cb44b537712a55b062a6df149be1250bd5a7b996
MONDSKY <r6544083@gmail.com>
2025-10-25 07:09:52 +0800

    Update ThreeDimensionalShape.java

--- src/ThreeDimensionalShape.java
+++ src/ThreeDimensionalShape.java
@@ -1,1 +1,10 @@
+public abstract class ThreeDimensionalShape extends Shape {
+
+    // displays 3D shape details
+    public void displayDetails() {
+        System.out.println("Shape: " + getName());
+        System.out.printf("Surface Area: %.2f%n", getArea());
+        System.out.printf("Volume: %.2f%n%n", getVolume());
+    }
+}
 
