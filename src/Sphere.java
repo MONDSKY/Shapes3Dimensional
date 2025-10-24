c03cacc3e25bfa0d21d8907168f4bd2044d17ec8
MONDSKY <r6544083@gmail.com>
2025-10-25 06:54:48 +0800

    Update Sphere.java

--- src/Sphere.java
+++ src/Sphere.java
@@ -1,1 +1,35 @@
 
+public class Sphere extends ThreeDimensionalShape {
+    private double radius;
+
+    public Sphere(double radius) {
+        setRadius(radius);
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        if (radius <= 0) {
+            throw new IllegalArgumentException("Radius must be positive.");
+        }
+        this.radius = radius;
+    }
+
+    @Override
+    public String getName() {
+        return "Sphere";
+    }
+
+    @Override
+    public double getArea() {
+        return 4 * Math.PI * Math.pow(radius, 2);
+    }
+
+    @Override
+    public double getVolume() {
+        return (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+    }
+}
+
