23fddb0b5e533a24e7d61e86fd42f856f938e45d
MONDSKY <r6544083@gmail.com>
2025-10-25 07:08:21 +0800

    Update Cube.java

--- src/Cube.java
+++ src/Cube.java
@@ -1,1 +1,33 @@
+public class Cube extends ThreeDimensionalShape {
+    private double side;
 
+    public Cube(double side) {
+        setSide(side);
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        if (side <= 0) {
+            throw new IllegalArgumentException("Side length must be positive.");
+        }
+        this.side = side;
+    }
+
+    @Override
+    public String getName() {
+        return "Cube";
+    }
+
+    @Override
+    public double getArea() {
+        return 6 * Math.pow(side, 2);
+    }
+
+    @Override
+    public double getVolume() {
+        return Math.pow(side, 3);
+    }
+}
